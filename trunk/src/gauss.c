213fde71e5600ebf80b7c146201df0fe98b735ba
ZupaPomidorowa <95253491+ZupaPomidorowa@users.noreply.github.com>
2021-12-21 22:50:19 +0100

    Update gauss.c
    Poprawki

--- trunk/src/gauss.c
+++ trunk/src/gauss.c
@@ -1,39 +1,24 @@
 #include "gauss.h"
-#include <math.h>
+#include <stdio.h>
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
+	int i,j,k;
+	int g=0;
+	for(i=0; i<mat->c; i++){
+		if((mat->data[i][g])==0)
+			return 1;
+		for(j=i+1; j<mat->c; j++){
+			for(k=g; k<mat->r;k++)
+				mat->data[j][k]=(mat->data[j][k])/(mat->data[i][k]);
+			b->data[j][0]==(b->data[j][0])/(b->data[i][0]);
+		}
+		g++;
+	}
 
-        double **ma=mat->data;   //macierz
-        double **mar=b->data;   //rozszerzenie macierzy
-        double *tem, tem2, factor;
-        int row = mat->r;
-        int column = mat->c;
-        int i, j, q, p, max;
-
-        for(i=0; i<column-1; i++)
-        {
- 
-		for(q=i+1; q<row; q++)
-                {
-                        if(ma[0][0] != 0)
-                        {
-                                factor = ma[q][i]/ma[i][i];
-                                for(p=0; p<column; p++)
-                                        {
-                                        ma[q][p] -= ma[i][p]*factor;
-                                        mar[q][0] -= mar[i][0]*factor;
-                                        }
-                        }
-                        return 1;
-                }
-
-        }
-
-        return 0;
+		return 0;
 }
-
 
